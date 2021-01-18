216e05fb1bbf2d53519dcd334e62fe6dce977565
choco <choco@choco.choco>
2021-01-18 13:53:16 +0800

    20210118 commit -get strange result after xor

--- PaddingOracle/code.py
+++ PaddingOracle/code.py
@@ -13,17 +13,20 @@ mod_cipher = list(cipher)
 print(len(mod_cipher))
 #print(type(mod_cipher))
 
+temp_cipher = list(cipher)
+
 text = [0] * 64
 
-# 8th block
+# seven blocks
 for block in range(7):
     prev_block_idx = 8*(7-block) - 1
+    # 8 bytes in one block
     for rounds in range(8):
-        mod_cipher = list(cipher)
+        mod_cipher = list(temp_cipher)
         if (rounds == 0):
             for i in range(256):
                 mod_cipher[prev_block_idx] = i
-                try_cipher_enc = pybase64.b64encode(bytes(mod_cipher)).decode('utf-8')
+                try_cipher_enc = pybase64.b64encode(bytes(mod_cipher)).decode('ASCII')
             #    print(try_cipher_enc)
                 conn.send(try_cipher_enc)
             #    print("send text")
@@ -35,28 +38,63 @@ for block in range(7):
                 if (s1[2] == 'C'):
                     print("found i = " + str(i))
                     break
-            print(mod_cipher)
+            #print(mod_cipher)
 
             # last byte is i xor 1
             text[prev_block_idx+8] = i ^ 1
         else:
             for i in range(rounds):
-                index = prev_block_idx - rounds
+                index = prev_block_idx - i
                 for j in range(256):
-                    if (j ^ text[index+8]) == 2:
+                    if (j ^ text[index+8]) == (rounds + 1):
                         mod_cipher[index] = j
                         break
+            index = prev_block_idx - rounds
             for j in range(256):
                 mod_cipher[index] = j
-                try_cipher_enc = pybase64.b64encode(bytes(mod_cipher)).decode('utf-8')
+                try_cipher_enc = pybase64.b64encode(bytes(mod_cipher)).decode('ASCII')
                 conn.send(try_cipher_enc)
                 conn.send('\n')
                 s1 = conn.recvlineS()
                 if (s1[2] == 'C'):
                     print("found j = " + str(j))
                     break
-            print(mod_cipher)
+            #print(mod_cipher)
             text[index+8] = j ^ (rounds + 1)
+
+init = [0]*8
+prev_block_idx = 7
+
+for rounds in range(8):
+    mod_cipher = list(cipher)
+    if (rounds == 0):
+        for i in range(256):
+            mod_cipher[prev_block_idx] = i
+            try_cipher_enc = pybase64.b64encode(bytes(mod_cipher)).decode('ASCII')
+            conn.send(try_cipher_enc)
+            conn.send('\n')
+            s1 = conn.recvlineS()
+            if (s1[2] == 'C'):
+                print("found i = " + str(i))
+                break
+        init[prev_block_idx] = i ^ 1
+    else:
+        for i in range(rounds):
+            index = prev_block_idx - rounds
+            for j in range(256):
+                if (j ^ init[index]) == (rounds + 1):
+                    mod_cipher[index] = j
+                    break
+        for j in range(256):
+            mod_cipher[index] = j
+            try_cipher_enc = pybase64.b64encode(bytes(mod_cipher)).decode('ASCII')
+            conn.send(try_cipher_enc)
+            conn.send('\n')
+            s1 = conn.recvlineS()
+            if (s1[2] == 'C'):
+                print("found j = " + str(j))
+                break
+        init[index] = j ^ (rounds + 1)
 
 mod_cipher = list(cipher)
 new = [0]*64
@@ -71,7 +109,9 @@ for i in range(64):
 
 print(new)
 
-print(pybase64.b64decode(bytes(new)).decode('utf-8'))
+for item in new:
+    if (item >= 32 and item <= 126):
+        print(chr(item))
 
 
 
